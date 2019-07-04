457a9e340b77529805fff8739a8118ec8dc81770
Eduardo <feduardo191@gmail.com>
2019-07-04 14:48:38 -0300

    Setup fetch data in database

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import 'normalize.css/normalize.css';
 import AppRouter from './routers/AppRouter';
 import './styles/style.scss';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import { Provider } from 'react-redux';
@@ -19,5 +19,11 @@ const jsx = (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+
 
--- src/tests/fixtures/expenses.js
+++ src/tests/fixtures/expenses.js
@@ -1,19 +1,22 @@
 import moment from 'moment';
 
 const expenses = [{
-    id: 1,
+    id: '1',
     description: 'Gum',
     amount: 195,
+    note: 'my note',
     createdAt: 0
 }, {
-    id: 2,
+    id: '2',
     description: 'Rent',
     amount: 109500,
+    note: 'my note',
     createdAt: moment(0).subtract(4,'days').valueOf()
 }, {
-    id: 3,
+    id: '3',
     description: 'Credit Card',
     amount: 4500,
+    note: 'my note',
     createdAt: moment(0).add(4, 'days').valueOf()
 }]
 
